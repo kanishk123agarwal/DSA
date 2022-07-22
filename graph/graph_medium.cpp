1fd0a7edcd65d27889299d256faa0b1f7791f3b7
kanishk123agarwal <kanishkagarwal208@gmail.com>
2022-07-22 11:54:28 +0530

    topological sort

--- graph/graph_medium.cpp
+++ graph/graph_medium.cpp
@@ -1,3 +1,20 @@
+// QUES 1 :- CYCLE DETECTION IN UNDIRECTED GRAPH BY USING
+// 1)BFS
+// 2)DFS
+
+
+// QUES 2 :- CYCLE DETECTION IN DIRECTED GRAPH BY USING 
+// 1) DFS 
+// 2) BFS 
+
+
+// QUES 3 :- TOPOLOGICAL SORT 
+// 1) DFS
+// 2) BFS
+
+
+
+
 
 // check the cycle in undirected graph using bfs and dfs
 https://www.codingninjas.com/codestudio/problems/cycle-detection-in-undirected-graph_1062670?topList=love-babbar-dsa-sheet-problems&leftPanelTab=1&utm_source=youtube&utm_medium=affiliate&utm_campaign=Lovebabbar
@@ -260,5 +277,260 @@ int detectCycleInDirectedGraph(int n, vector < pair < int, int >> & edges) {
 44
     return false;
 45
+}
+
+
+
+// topological sort by using bfs and dfs
+Topological Sort
+
+Problem Statement
+A Directed Acyclic Graph (DAG) is a directed graph that contains no cycles.
+Topological Sorting of DAG is a linear ordering of vertices such that for every directed edge from vertex ‘u’ to vertex ‘v’, vertex ‘u’ comes before ‘v’ in the ordering. Topological Sorting for a graph is not possible if the graph is not a DAG.
+Given a DAG consisting of ‘V’ vertices and ‘E’ edges, you need to find out any topological sorting of this DAG. Return an array of size ‘V’ representing the topological sort of the vertices of the given DAG.
+For example, Consider the DAG shown in the picture.
+
+In this graph, there are directed edges from 0 to 1 and 0 to 3, so 0 should come before 1 and 3. Also, there are directed edges from 1 to 2 and 3 to 2 so 1 and 3 should come before 2.
+So, The topological sorting of this DAG is {0 1 3 2}.
+Note that there are multiple topological sortings possible for a DAG. For the graph given above one another topological sorting is: {0, 3, 1, 2}
+Note:
+1. It is guaranteed that the given graph is DAG.
+2. There will be no multiple edges and self-loops in the given DAG.
+3. There can be multiple correct solutions, you can find any one of them. 
+4. Don’t print anything, just return an array representing the topological sort of the vertices of the given DAG.
+Input Format:
+The first line of input contains an integer ‘T’ denoting the number of test cases. The description of  ‘T’ test cases follows.
+
+The first line of each test case contains two space-separated integers ‘V’, ‘E’, representing the number vertices and edges in the graph respectively.
+
+Then ‘E’ lines follow,  each containing 2 space-separated integers ‘u’, ‘v’ representing that there is a directed edge from vertex ‘u’ to vertex ‘v’
+Output Format :
+For each test case, return an array representing the topological sort of the vertices of the given DAG.
+Note :
+You do not need to print anything. It has already been taken care of. Just implement the given function.
+Constraints:
+1 <= T <= 50
+1 <= V <= 10^4
+0 <= E <= 10^4
+0 <= u, v < V 
+
+Where ‘T’ is the total number of test cases, ‘V’ is the number of vertices, ‘E’ is the number of edges, and ‘u’ and ‘v’ both represent the vertex of a given graph.
+
+Time limit: 2 sec
+Sample Input 1:
+2
+2 1
+1 0
+3 2
+0 1
+0 2
+Sample Output 1:
+1 0
+0 2 1
+Explanation Of Sample Input 1:
+Test case 1:
+The number of vertices ‘V’ = 2 and number of edges ‘E’ = 1.
+The graph is shown in the picture: 
+
+The topological sorting of this graph should be {1, 0}  as there is a directed edge from vertex 1 to vertex 0, thus 1 should come before 0 according to the given definition of topological sorting.
+
+Test case 2:
+The number of vertices ‘V’ = 3 and number of edges ‘E’ = 2.
+The graph is shown in the picture:
+
+As there are two directed edges starting from 0, so 0 should come before 1 and 2 in topological sorting. 
+Thus the topological sorting of this graph should be {0, 2, 1} or {0, 1, 2}
+Sample Input 2:
+2
+1 0
+4 4
+0 1
+0 3
+1 2
+3 2
+Sample Output 2:
+0
+0 1 3 2
+Explanation Of Sample Input 2:
+Test case 1:   
+There is only a single vertex in the graph that is 0, so its topological sort will be {0}.
+
+Test case 2:
+See problem statement for its explanation
+Previous
+Next
+ 
+}
+1
+#include<unordered_map>
+2
+#include<list>
+3
+#include<stack>
+4
+#include<queue>
+5
+#include<vector>
+6
+​
+7
+// using DEPTH FIRST SEARCH
+8
+void topoSort(int node,vector<bool> &visited,stack<int> &s,unordered_map<int,list<int> > &adj){
+9
+    visited[node]=true;
+10
+    for(auto neighbour:adj[node]){
+11
+        if(!visited[neighbour]){
+12
+            topoSort(neighbour,visited,s,adj);
+13
+        }
+14
+    }
+15
+    s.push(node);
+16
+}
+17
+​
+18
+vector<int> topologicalSort(vector<vector<int>> &edges, int v, int e)  {
+19
+    // Write your code here
+20
+//     create an adjacent list
+21
+    unordered_map<int,list<int> > adj;
+22
+    for(int i=0;i<e;i++){
+23
+        int u=edges[i][0];
+24
+        int v=edges[i][1];
+25
+        
+26
+        adj[u].push_back(v);
+27
+    }
+28
+    
+29
+//     Handle all the different component in the graph
+30
+//     using dfs  
+31
+//     T.C O(V+E)
+32
+//     S.C O(V)
+33
+//     vector<bool> visited(v);
+34
+//     stack<int> s;
+35
+//     for(int i=0;i<v;i++){
+36
+//         if(!visited[i]){
+37
+//             topoSort(i,visited,s,adj);
+38
+//         }
+39
+//     }
+40
+//     vector<int> ans;
+41
+//     while(!s.empty()){
+42
+//         ans.push_back(s.top());
+43
+//         s.pop();
+44
+//     }
+45
+//     return ans;
+46
+    
+47
+    
+48
+//     using bfs
+49
+//     T.C O(N+E)
+50
+//     S.C O(N+E)
+51
+//     find all indegree 
+52
+    vector<int> indegree(v);
+53
+    for(auto i: adj){
+54
+        for(auto j: i.second)
+55
+        {
+56
+            indegree[j]++;
+57
+        }
+58
+    }
+59
+    
+60
+//     0 indegree waaloko push krna hai
+61
+    queue<int> q;
+62
+    vector<int> ans;
+63
+    for(int i=0;i<v;i++){
+64
+        if(indegree[i]==0){
+65
+            q.push(i);
+66
+        }
+67
+    }
+68
+    
+69
+//     do bfs
+70
+    while(!q.empty()){
+71
+        int front=q.front();
+72
+        q.pop();
+73
+        
+74
+//         ans store krwa lo 
+75
+        ans.push_back(front);
+76
+        
+77
+//         update the indegree of neighbour
+78
+        for(auto neighbour: adj[front]){
+79
+            indegree[neighbour]--;
+80
+            if(indegree[neighbour] == 0){
+81
+                q.push(neighbour);
+82
+            }
+83
+        }
+84
+    }
+85
+    return ans;
+86
 }
 
