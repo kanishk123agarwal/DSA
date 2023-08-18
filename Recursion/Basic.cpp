9795d20cde7500ed1ba5cbae7be0c9b12fd94aaf
kanishk123agarwal <kanishkagarwal208@gmail.com>
2023-08-18 12:03:30 +0530

    add question

--- Recursion/Basic.cpp
+++ Recursion/Basic.cpp
@@ -42,6 +42,33 @@ int findMax(int arr[],int n){
     return (arr[n-1]<max)?max:arr[n-1];
 }
 
+// Ques6:sum of prime between the range
+bool isPrime(int number,int divisor=2){
+    if(number<=2){
+        return (number==2);
+    }
+    if(number%divisor==0) return false;
+    if(divisor*divisor > number) return true;
+
+    return isPrime(number,divisor+1);
+}
+int sumPrime(int start,int end){
+    if(start>end){
+        return 0;
+    }
+    int sum=isPrime(start)?start:0;
+    return sum+sumPrime(start+1,end);
+}
+
+// Ques7:sum of odd and even no between the range
+void sumEvenOdd(int start,int end,int &evenS,int &oddS){
+    if(start>end) return;
+
+    if(start%2==0) evenS=evenS+start;
+    else oddS=oddS+start;
+    sumEvenOdd(start+1,end,evenS,oddS);
+}
+
 int main(){
     int arr[5]={5,3,10,4,2};
     // int sum=sumArray(arr,0,5);
@@ -63,4 +90,13 @@ int main(){
     // Ques5:
     int max=findMax(arr,5);
     cout<<max<<endl;
+
+    // ques6:sum of prime no between the range
+    int sumP = sumPrime(1,20);
+    cout<<sumP<<endl;
+
+    // Ques7: 
+    int evenS=0,oddS=0;
+    sumEvenOdd(10,20,evenS,oddS);
+    cout<<evenS<<" "<<oddS<<endl;
 }
