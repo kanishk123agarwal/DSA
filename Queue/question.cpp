cc8bd6b8adc50f4e448a7c5afc261947150da8d6
kanishk123agarwal <kanishkagarwal208@gmail.com>
2023-09-03 23:43:13 +0530

    question add

--- Queue/question.cpp
+++ Queue/question.cpp
@@ -1,9 +1,12 @@
 #include<iostream>
 #include<queue>
 #include<stack>
+#include<map>
+#include<unordered_map>
 using namespace std;
 
 // reverse the queue using stack
+https://practice.geeksforgeeks.org/problems/queue-reversal/1
 queue<int> rev(queue<int> q)
 {
     // add code here.
@@ -23,6 +26,7 @@ queue<int> rev(queue<int> q)
 }
 
 // first negative number for every k window
+https://practice.geeksforgeeks.org/problems/first-negative-integer-in-every-window-of-size-k3345/1
 vector<long long> printFirstNegativeInteger(long long int A[],
                                              long long int N, long long int K) {
                                                             
@@ -66,6 +70,7 @@ vector<long long> printFirstNegativeInteger(long long int A[],
  }
 
 // Function to reverse first k elements of a queue.
+https://practice.geeksforgeeks.org/problems/reverse-first-k-elements-of-queue/1
 queue<int> modifyQueue(queue<int> q, int k) {
     // add code here.
     stack<int> s;
@@ -87,6 +92,53 @@ queue<int> modifyQueue(queue<int> q, int k) {
     }
     return q;
 }
+
+https://practice.geeksforgeeks.org/problems/first-non-repeating-character-in-a-stream1216/1
+string FirstNonRepeating(string A){
+		    // Code here
+		    string ans="";
+		    unordered_map<char,int> count;
+		    queue<int> q;
+		    for(int i=0;i<A.length();i++){
+		        char ch=A[i];
+		        count[ch]++;
+		        q.push(ch);
+		        while(!q.empty()){
+		            if(count[q.front()] > 1)
+		               q.pop();
+		            else{
+		               ans.push_back(q.front());
+		               break;
+		            }
+		        }
+		        if(q.size()==0){
+		            ans.push_back('#');
+		        }
+		    }
+		    return ans;
+		}
+
+
+// circular tour
+https://practice.geeksforgeeks.org/problems/circular-tour-1587115620/1
+int tour(petrolPump p[],int n)
+    {
+       //Your code here
+       int deficit=0;
+       int balance=0,start=0;
+       for(int i=0;i<n;i++){
+           balance+=p[i].petrol-p[i].distance;
+           if(balance<0){
+               deficit+=balance;
+               start=i+1;
+               balance=0;
+           }
+       }
+       if(balance+deficit>=0)
+        return start;
+       else
+        return -1;
+    }
 
 int main(){
     int n=6;
